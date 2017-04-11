00f8a0077c8810865aac5c2216c2aa2c275fd695
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-04-11 09:46:53 +0200

    finished hom,team and various links for the page

--- config.rb
+++ config.rb
@@ -88,4 +88,8 @@ activate :deploy do |deploy|
   # deploy.branch = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+["Jaina", "Anduin", "Thrall", "Garrosh", "Malfurion", "Guldan"].each do |player|
+  proxy "/cards/#{player}.html", "/cards/show.html", :locals => { :owner => player }, :ignore => true
+end
+
 end
